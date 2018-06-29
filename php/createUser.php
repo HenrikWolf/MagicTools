fd47ad86fb85956cdfa90c669d3ef0ac7a023d49
HenrikWolf <mail@henrikwolf.de>
2018-06-29 17:34:58 +0200

    createUser: write auth token (not just username and password) into database. #18

--- php/createUser.php
+++ php/createUser.php
@@ -4,7 +4,7 @@ require_once 'dbConnect.php';
 
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = $err = "";
+$username_err = $password_err = $confirm_password_err = $token_err = "";
 
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -61,33 +61,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   // Validate auth_token_set
   if (empty(trim($_POST["app_token"]))) {
-    $err = "Please enter an app token";
+    $token_err = "Please enter an app token";
   } elseif (empty(trim($_POST["app_token"]))) {
-    $err = "Please enter an app token";
+    $token_err = "Please enter an app token";
   } elseif (empty(trim($_POST["app_token_secret"]))) {
-    $err = "Please enter an app token secret";
+    $token_err = "Please enter an app token secret";
   } elseif (empty(trim($_POST["access_token"]))) {
-    $err = "Please enter an access token";
+    $token_err = "Please enter an access token";
   } elseif (empty(trim($_POST["access_token_secret"]))) {
-    $err = "Please enter an access token secret";
+    $token_err = "Please enter an access token secret";
   }
 
   // Check input errors before inserting in database
-  if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($err)) {
+  if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($token_err)) {
     // Prepare an insert statement
-    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $sql = "INSERT INTO users (username, password, app_token, app_token_secret, access_token, access_token_secret) VALUES (?,?,?,?,?,?)";
     if ($stmt = mysqli_prepare($link, $sql)) {
       // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+      mysqli_stmt_bind_param($stmt, "ssssss", $param_username, $param_password, $param_app_token,
+            $param_app_token_secret, $param_access_token, $param_access_token_secret);
 
       // Set parameters
       $param_username = $username;
       $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+      $param_app_token = trim($_POST['app_token']);
+      $param_app_token_secret = trim($_POST['app_token_secret']);
+      $param_access_token = trim($_POST['access_token']);
+      $param_access_token_secret = trim($_POST['access_token_secret']);
 
       // Attempt to execute the prepared statement
       if (mysqli_stmt_execute($stmt)) {
-        $succ = "Prima! Der Benutzer wurde angelegt!";
-        echo json_encode(array('succ' => $succ));
+        echo json_encode(array('succ' => "Prima! Der Benutzer wurde angelegt!"));
       } else {
         echo json_encode(array('err' => "Something went wrong. Please try again later."));
       }
@@ -100,8 +104,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo json_encode(array('err' => $password_err));
   } elseif ($confirm_password_err) {
     echo json_encode(array('err' => $confirm_password_err));
-  } elseif ($err) {
-    echo json_encode(array('err' => $err));
+  } elseif ($token_err) {
+    echo json_encode(array('err' => $token_err));
   }
   // Close connection
   mysqli_close($link);
