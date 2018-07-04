5cdf59303e32e03877e03f55d044776bc5ffc917
HenrikWolf <mail@henrikwolf.de>
2018-07-04 11:42:08 +0200

    remove getLists button, beautiful form and code clean up. #25, #7

--- content/export.php
+++ content/export.php
@@ -2,45 +2,39 @@
 <!-- Begin Export -->
 <div class="tab-pane fade show active" id="export" role="tabpanel" aria-labelledby="export-tab">
   <h3>Export Wantlist</h3>
-  <div class="row">
-    <div class="col-md-6"></div>
-    <div class="col-md-3">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-get-lists">Get Lists</button>
+
+  <form class="mt-md-4">
+
+    <!-- Beginn Selection for List and Button -->
+    <div class="form-group row">
+      <div class="col-md-6">
+        <select id="export-dropdown" class="form-control" disabled>
+          <option value="null" selected>Choose Wantlist</option>
+        </select>
+      </div>
+      <div class="col-md-3">
+        <button type="button" class="btn btn-secondary btn-block" id="btn-get-wants">Get Wants</button>
+      </div>
+      <div class="col-md-3 center-parent-dropdown">
+        <i id="icon-export-get-wants" class="fas fa-lg center-child-dropdown"></i>
+      </div>
     </div>
-    <div class="col-md-3 center-parent-regular">
-      <i id="icon-export-get-lists" class="fas fa-lg center-child-regular"></i>
+
+    <!-- Beginn Output Wants -->
+    <div class="form-group">
+      <textarea class="form-control" id="export-output" rows="12" readonly></textarea>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-md-6">
-      <label for="export-dropdown"></label>
-      <select id="export-dropdown" class="form-control" disabled>
-        <option value="null" selected>Choose Wantlist</option>
-      </select>
+
+    <!-- Beginn Button and Alert -->
+    <div class="form-group row">
+      <div class="col-md-10">
+        <div class="alert alert-danger alert-trim" role="alert" style="display:none;" id="alert-export"></div>
+      </div>
+      <div class="col-md-2" align="right">
+        <button type="button" class="btn btn-secondary btn-block" id="btn-copy-clipboard">Copy</button>
+      </div>
     </div>
-    <div class="col-md-3 pt-4">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-get-wants">Get Wants</button>
-    </div>
-    <div class="col-md-3 center-parent-dropdown">
-      <i id="icon-export-get-wants" class="fas fa-lg center-child-dropdown"></i>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md">
-      <form>
-        <div class="form-group">
-          <label for="export-output"></label>
-          <textarea class="form-control" id="export-output" rows="10" readonly></textarea>
-        </div>
-      </form>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md" align="left">
-      <div class="alert alert-danger alert-trim" role="alert" style="display:none;" id="alert-export"></div>
-    </div>
-    <div class="col-md-2" align="right">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-copy-clipboard">Copy</button>
-    </div>
-  </div>
+
+  </form>
+
 </div>
