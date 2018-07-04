a36063547310ab103fa609f512eafd54d170daea
HenrikWolf <mail@henrikwolf.de>
2018-07-04 11:14:50 +0200

    beautify forms and code clean up. #18, #7

--- content/userCreate.php
+++ content/userCreate.php
@@ -1,54 +1,59 @@
 
 <!-- Begin User-Create -->
-<div class="tab-pane fade" id="user-create" role="tabpanel" aria-labelledby="user-edit-tab">
-  <h3>Authentifizierung</h3>
+<div class="tab-pane fade" id="user-create" role="tabpanel" aria-labelledby="user-create-tab">
+  <h3>Create an account</h3>
   <p class="lead">Please fill this form to create an account.</p>
-  <div class="row">
-    <div class="col-md">
-      <form>
-        <div class="form-group">
-          <label for="user-create-username">Username</label>
-          <input id="user-create-username" type="text" class="form-control" aria-describedby="help-username" placeholder="Username">
-        </div>
-        <div class="form-group">
-          <label for="user-create-password">Password</label>
-          <input id="user-create-password" type="password" class="form-control" aria-describedby="help-password" placeholder="Password">
-        </div>
-        <div class="form-group">
-          <label for="user-create-confirm-password">Confirm Password</label>
-          <input id="user-create-confirm-password" type="password" class="form-control" aria-describedby="help-confirm-password" placeholder="Confirm Password">
-        </div>
-        <div class="form-group">
-          <label for="user-create-app-token">App Token</label>
-          <input id="user-create-app-token" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="App Token">
-        </div>
-        <div class="form-group">
-          <label for="user-create-app-token-secret">App Token Secret</label>
-          <input id="user-create-app-token-secret" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="App Token Secret">
-        </div>
-        <div class="form-group">
-          <label for="user-create-access-token">Access Token</label>
-          <input id="user-create-access-token" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="Access Token">
-        </div>
-        <div class="form-group">
-          <label for="user-create-access-token-secret">Access Token Secret</label>
-          <input id="user-create-access-token-secret" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="Access Token Secret">
-        </div>
-      </form>
+
+  <form class="mt-md-4">
+
+    <!-- Beginn Input username and password -->
+    <div class="form-group">
+      <label for="user-create-username">Username</label>
+      <input id="user-create-username" type="text" class="form-control" placeholder="Username">
     </div>
-  </div>
-  <div class="row justify-content-center">
-    <div class="col-md-3">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-user-create-check">Check Tokens</button>
+    <div class="form-group">
+      <label for="user-create-password">Password</label>
+      <input id="user-create-password" type="password" class="form-control" placeholder="Password">
     </div>
-    <div class="col-md-2">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-user-create-save">Save</button>
+    <div class="form-group">
+      <label for="user-create-confirm-password">Confirm Password</label>
+      <input id="user-create-confirm-password" type="password" class="form-control" placeholder="Confirm Password">
     </div>
-    <div class="col-md-1 center-parent-regular">
-      <i id="icon-user-create" class="fas fa-lg center-child-regular"></i>
+
+    <!-- Beginn Input auth token set -->
+    <div class="form-group">
+      <label for="user-create-app-token">App Token</label>
+      <input id="user-create-app-token" typ="text" class="form-control" placeholder="App Token">
     </div>
-    <div class="col-md-6" align="right">
-      <div class="alert alert-success alert-trim" role="alert" style="display:none;" id="alert-user-create"></div>
+    <div class="form-group">
+      <label for="user-create-app-token-secret">App Token Secret</label>
+      <input id="user-create-app-token-secret" typ="text" class="form-control" placeholder="App Token Secret">
     </div>
-  </div>
+    <div class="form-group">
+      <label for="user-create-access-token">Access Token</label>
+      <input id="user-create-access-token" typ="text" class="form-control" placeholder="Access Token">
+    </div>
+    <div class="form-group">
+      <label for="user-create-access-token-secret">Access Token Secret</label>
+      <input id="user-create-access-token-secret" typ="text" class="form-control" placeholder="Access Token Secret">
+    </div>
+
+    <!-- Beginn buttons and alert -->
+    <div class="form-group row">
+      <div class="col-md-3">
+        <button type="button" class="btn btn-secondary btn-block" id="btn-user-create-check">Check Tokens</button>
+      </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-secondary btn-block" id="btn-user-create-save">Save</button>
+      </div>
+      <div class="col-md-1 center-parent-regular">
+        <i id="icon-user-create" class="fas fa-lg center-child-regular"></i>
+      </div>
+      <div class="col-md-6" align="right">
+        <div class="alert alert-success alert-trim" role="alert" style="display:none;" id="alert-user-create"></div>
+      </div>
+    </div>
+
+  </form>
+
 </div>
--- content/userEdit.php
+++ content/userEdit.php
@@ -1,42 +1,45 @@
 
 <!-- Begin User-Edit -->
 <div class="tab-pane fade" id="user-edit" role="tabpanel" aria-labelledby="user-edit-tab">
-  <h3>Authentifizierung</h3>
-  <p class="lead" id="p-user-edit"><p>
-  <div class="row">
-    <div class="col-md">
-      <form>
-        <div class="form-group">
-          <label for="user-edit-app-token">App Token</label>
-          <input id="user-edit-app-token" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="App Token">
-        </div>
-        <div class="form-group">
-          <label for="user-edit-app-token-secret">App Token Secret</label>
-          <input id="user-edit-app-token-secret" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="App Token Secret">
-        </div>
-        <div class="form-group">
-          <label for="user-edit-access-token">Access Token</label>
-          <input id="user-edit-access-token" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="Access Token">
-        </div>
-        <div class="form-group">
-          <label for="user-edit-access-token-secret">Access Token Secret</label>
-          <input id="user-edit-access-token-secret" typ="text" class="form-control" aria-describedby="help-access-token" placeholder="Access Token Secret">
-        </div>
-      </form>
+  <h3>Edit your account</h3>
+  <p class="lead" id="p-user-edit"></p>
+
+  <form class="mt-md-4">
+
+    <!-- Beginn Input auth token set -->
+    <div class="form-group">
+      <label for="user-edit-app-token">App Token</label>
+      <input id="user-edit-app-token" typ="text" class="form-control" placeholder="App Token">
     </div>
-  </div>
-  <div class="row justify-content-center">
-    <div class="col-md-3">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-user-edit-check">Check Tokens</button>
+    <div class="form-group">
+      <label for="user-edit-app-token-secret">App Token Secret</label>
+      <input id="user-edit-app-token-secret" typ="text" class="form-control" placeholder="App Token Secret">
     </div>
-    <div class="col-md-2">
-      <button type="button" class="btn btn-secondary btn-block" id="btn-user-edit-save">Save</button>
+    <div class="form-group">
+      <label for="user-edit-access-token">Access Token</label>
+      <input id="user-edit-access-token" typ="text" class="form-control" placeholder="Access Token">
     </div>
-    <div class="col-md-1 center-parent-regular">
-      <i id="icon-user-edit" class="fas fa-lg center-child-regular"></i>
+    <div class="form-group">
+      <label for="user-edit-access-token-secret">Access Token Secret</label>
+      <input id="user-edit-access-token-secret" typ="text" class="form-control" placeholder="Access Token Secret">
     </div>
-    <div class="col-md-6" align="right">
-      <div class="alert alert-success alert-trim" role="alert" style="display:none;" id="alert-user-edit"></div>
+
+    <!-- Beginn buttons and alert -->
+    <div class="form-group row">
+      <div class="col-md-3">
+        <button type="button" class="btn btn-secondary btn-block" id="btn-user-edit-check">Check Tokens</button>
+      </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-secondary btn-block" id="btn-user-edit-save">Save</button>
+      </div>
+      <div class="col-md-1 center-parent-regular">
+        <i id="icon-user-edit" class="fas fa-lg center-child-regular"></i>
+      </div>
+      <div class="col-md-6" align="right">
+        <div class="alert alert-success alert-trim" role="alert" style="display:none;" id="alert-user-edit"></div>
+      </div>
     </div>
-  </div>
+
+  </form>
+
 </div>
