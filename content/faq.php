9d28a327c8792c1b499d9ce17a0bb577f7fc436f
Rolfus187 <rolfus187@googlemail.com>
2018-08-06 16:55:05 +0200

    small changes to FAQ for #26

--- content/faq.php
+++ content/faq.php
@@ -7,13 +7,25 @@
       <div class="panel panel-default ">
         <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question0">
           <h4 class="panel-title">
-            <a href="#" class="ing user-link-faq">How do I get the MKM-Tokens?</a>
+            <a href="#" class="ing user-link-faq">What does this website do?</a>
           </h4>
         </div>
         <div id="question0" class="panel-collapse collapse" style="height: 0px;">
           <div class="panel-body">
+            <p>This website is a collection of small tools using the API of <a href="https://www.cardmarket.com/de/Magic" class="label label-success">cardmarket.com</a>. As the cardmarket lacks some basic functionalities, such as exporting wantlists, those were implemented here.</p>
+            <p>Currently, a tool to export wantlists is implemented. In addition, there is a work-in-progress version of an import module.</p>
+          </div>
+        </div>
+        <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question1">
+          <h4 class="panel-title">
+            <a href="#" class="ing user-link-faq">How do I use the MKM tools?</a>
+          </h4>
+        </div>
+        <div id="question1" class="panel-collapse collapse" style="height: 0px;">
+          <div class="panel-body">
             <p>To get the tokens needed for using this tool, this option has to be activated in your user profile on <a href="https://www.cardmarket.com/de/Magic" class="label label-success">cardmarket.com</a> first.</p>
-            <p>Access your <a href="https://www.cardmarket.com/de/Magic/MainPage/showMyAccount" class="label label-success">profile</a> and scroll down to WIEAUCHIMMERDASGENAUHEIßT. Usw... will hier auch am Liebsten ein Bild einfügen wie das ganze aussieht.
+            <p>Access your <a href="https://www.cardmarket.com/de/Magic/MainPage/showMyAccount" class="label label-success">profile</a> and scroll down to WIEAUCHIMMERDASGENAUHEIßT. Usw... will hier auch am Liebsten ein Bild einfügen wie das ganze aussieht.</p>
+            <p>Once you finished the activation on <a href="https://www.cardmarket.com/de/Magic" class="label label-success">cardmarket.com</a>, you can create an account using the button in the header. Just insert your tokens, chose a username and password and you are ready to go!</p>
           </div>
         </div>
       </div>
